f738016adaceb9c264a7f421712c5b3bcf1e68ed
NataliiaZinchenko <nataellie@gmail.com>
2024-03-18 11:43:57 +0000

    sessions(), messageReceived(), handle_my_custom_event() are declared

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def sessions():
     return render_template('session.html')
 
 
+def messageReceived(methods=['GET', 'POST']):
+    print('Message has been received!')
+
+
+@socketio.on('My event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('Received my event: ' + str(json))
+    socketio.emit('My response ', json, callback=messageReceived)
 
 
 if __name__ == '__main__':
